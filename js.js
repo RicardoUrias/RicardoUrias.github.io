41056530ac1618d694b779387089bb91a33662fa
RicardoUrias <94211251+RicardoUrias@users.noreply.github.com>
2024-01-24 20:14:37 -0600

    Update js.js

--- js.js
+++ js.js
@@ -5,9 +5,9 @@ function login() {
     // Aquí puedes agregar la lógica de autenticación, por ejemplo, verificar en una base de datos
 
     // Validación de correo electrónico usando una expresión regular simple
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     
-    if (emailRegex.test(email) && password.length >= 6) {
+    if (emailRegex.test(email) && password.length >= 8) {
         alert("Inicio de sesión exitoso");
     } else {
         alert("Error en el inicio de sesión. Verifica tus credenciales.");
