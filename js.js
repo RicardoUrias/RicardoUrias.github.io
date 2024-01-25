e1a659a91dbf792aa22938f486d5834dddda824b
RicardoUrias <94211251+RicardoUrias@users.noreply.github.com>
2024-01-24 20:51:08 -0600

    Update js.js

--- js.js
+++ js.js
@@ -7,12 +7,15 @@ function login(event) {
     var email = emailInput.value;
     var password = passwordInput.value;
 
+    // Validación de correo electrónico usando una expresión regular simple
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     
     if (emailRegex.test(email) && password.length >= 8) {
         alert("Inicio de sesión exitoso");
+        // No es necesario return true; aquí, ya que la acción predeterminada se realizará
     } else {
         alert("Error en el inicio de sesión. Verifica tus credenciales.");
+        // Borra los datos en los campos de correo electrónico y contraseña
         emailInput.value = "";
         passwordInput.value = "";
     }
