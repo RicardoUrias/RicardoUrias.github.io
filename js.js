346c4dd4e31c1b1fb182d0f812cd5676a20e2a60
RicardoUrias <94211251+RicardoUrias@users.noreply.github.com>
2024-01-24 20:37:41 -0600

    Update js.js

--- js.js
+++ js.js
@@ -2,15 +2,15 @@ function login() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
 
-    // Aquí puedes agregar la lógica de autenticación, por ejemplo, verificar en una base de datos
-
     // Validación de correo electrónico usando una expresión regular simple
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     
     if (emailRegex.test(email) && password.length >= 8) {
         alert("Inicio de sesión exitoso");
+        return true;  // Envía el formulario
     } else {
         alert("Error en el inicio de sesión. Verifica tus credenciales.");
+        return false;  // No envía el formulario
     }
 }
 
